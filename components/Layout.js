c6652fdca6acb415c8dadc72c9d22855576dff30
Yassine Benzouine <yassinebenzouine82@gmail.com>
2023-01-31 00:59:25 +0300

    Layout fixing

--- components/Layout.js
+++ components/Layout.js
@@ -1,25 +1,62 @@
-
-
 import Navbar from "./Navbar";
-
+import Head from "next/head";
+import Script from "next/script";
+import Link from "next/link";
 
 const Layout = ({ children }) => {
-  
-
   return (
-    
-      
-        <>
-         <main class="main" id="top">
-          <div class="container-fluid px-0">
-          <Navbar/>
+    <>
+      <Head>
+
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossorigin=""
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Nunito+Sans:wght@300;400;600;700;800;900&amp;display=swap"
+          rel="stylesheet"
+        />
+
+        <link
+          rel="stylesheet"
+          href="https://unicons.iconscout.com/release/v4.0.0/css/line.css"
+        />
+        <link
+          href="assets/css/theme-rtl.min.css"
+          type="text/css"
+          rel="stylesheet"
+          id="style-rtl"
+        />
+        <link
+          href="assets/css/theme.min.css"
+          type="text/css"
+          rel="stylesheet"
+          id="style-default"
+        />
+        <link
+          href="assets/css/user-rtl.min.css"
+          type="text/css"
+          rel="stylesheet"
+          id="user-style-rtl"
+        />
+        <link
+          href="assets/css/user.min.css"
+          type="text/css"
+          rel="stylesheet"
+          id="user-style-default"
+        />
+      </Head>
+
+      <main class="main" id="top">
+        <div class="container-fluid px-0">
+          <Navbar />
           {children}
         </div>
-        </main>
-         
-        </>
-      )
-    }
-    
+      </main>
+    </>
+  );
+};
 
 export default Layout;
